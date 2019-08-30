298fdffaf6870588e6288441f94188642b3c5e82
pedroblossbraga <50064364+pedroblossbraga@users.noreply.github.com>
2019-08-30 14:48:57 -0300

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -14,16 +14,7 @@ def date_generator():
 def send_email_net():
     date_str = date_generator()
 
-    '''
-    If you'd like, you could make an part create a date string to select the days you want the email is sent, and schedule the task
-    An example: (inside the Try)
-    today = date_generator()
-    day = int(input('Which day would you like to send the email?' ))
-    if day == int(today[0:2]:
-        # content ...
-    ...
-    '''
-    # This example uses the 30th day. You can change it, or create a schedulated .bat to check if the current day is equal to a number, to send the email.
+    # This example uses the day of execution. You can change it, or create a schedulated .bat to check if the current day is equal to a number, to send the email.
     days = input('Which days would you like to send the email?' )
     days_list = days.split(",")
     
@@ -40,16 +31,6 @@ def send_email_net():
                 </html>
                 """
                 msg['Subject'] = 'Enter Subject'
-
-            elif int(day) == int(date_str[0:2]):
-                email_content = """
-		<html>
-		    <head>Title 2</head>
-		    <p>Content 2</p>
-		</html>
-		"""
-                msg['Subject'] = 'Enter Subject'
-                
 
             #insert user passoword and email, respectively in password and msg['From]
             password = "Your_Password"
